67c4e3f006811eb55552f6c9999369abbad74edf
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-04 15:26:18 +0300

    works on update password controller

--- controllers/auth/resetPassword.js
+++ controllers/auth/resetPassword.js
@@ -2,16 +2,18 @@ const User = require("../../models/User");
 
 const resetPassword = async (req, res) => {
     const {email} = req.body;
+    console.log(email);
     
     if (email) {
         await User.findOne({'email': email}).then((user) => {
             console.log(user);
-            if (!user) {
+            if (user) {
+                // send email from here
+                const link = `http://localhost:6000/api/v1/auth/password/reset?email=${user.email}`;
+            res.status(200).json({"message": "reset password", "link": link});
+            } else {
                 res.status(400).json({"message": "user not found"});
             }
-            // send email from here
-            const link = `http://localhost:6000/api/v1/auth/password/reset?email=${user.email}`;
-            res.status(200).json({"message": "reset password", "link": link});
         }).catch((e) => {
             console.log(e);
             res.status(400).json({"message": "something went wrong", "error": e.message});
