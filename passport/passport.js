e0914058ffb93984616ab7c26f977533e261615a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-04 13:19:42 +0300

    puts passport authentication in its own file

---
+++ passport/passport.js
@@ -0,0 +1,32 @@
+const passportAuthentication = (passport, LocalStrategy, bcrypt, User) => {
+    passport.use(new LocalStrategy({
+        usernameField: 'email'
+    },
+        async function (username, password, done) {
+            console.log(`username: ${username}, password: ${password}`);
+            await User.findOne({ 'email': username }).then(async (user) => {
+                console.log(user);
+                if (!user) { return done(null, false, {message: "user not found"});}
+                const verifyPassword = await bcrypt.compare(password, user.password);
+                if (!verifyPassword) { return done(null, false, {message: "incorrect password"}); }
+                return done(null, user);
+            }).catch((e) => {
+                console.log(e);
+                if (e) { return done(e, {message: "something went wrong"}); }
+            });
+        }
+    ));
+    passport.serializeUser(function(user, cb) {
+        process.nextTick(function() {
+          cb(null, { id: user._id, username: user.username });
+        });
+      });
+      
+      passport.deserializeUser(function(user, cb) {
+        process.nextTick(function() {
+          return cb(null, user);
+        });
+      });
+}
+
+module.exports = passportAuthentication;
--- routes/auth.js
+++ routes/auth.js
@@ -7,34 +7,8 @@ const User = require('../models/User');
 const login = require('../controllers/auth/login');
 const logout = require('../controllers/auth/logout');
 
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-},
-    async function (username, password, done) {
-        console.log(`username: ${username}, password: ${password}`);
-        await User.findOne({ 'email': username }).then(async (user) => {
-            console.log(user);
-            if (!user) { return done(null, false, {message: "user not found"});}
-            const verifyPassword = await bcrypt.compare(password, user.password);
-            if (!verifyPassword) { return done(null, false, {message: "incorrect password"}); }
-            return done(null, user);
-        }).catch((e) => {
-            console.log(e);
-            if (e) { return done(e, {message: "something went wrong"}); }
-        });
-    }
-));
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      cb(null, { id: user._id, username: user.username });
-    });
-  });
-  
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
-  });
+const passportAuthentication = require('../passport/passport');
+passportAuthentication(passport, LocalStrategy, bcrypt, User);
 
 const authRoute = express.Router();
 
