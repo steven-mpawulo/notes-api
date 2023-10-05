ccd12cf6f2d7d0c65927952ba86f9d94c5a28a84
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 10:33:31 +0300

    works on get all users controller

---
+++ controllers/user/getUsers.js
@@ -0,0 +1,17 @@
+const User = require("../../models/User")
+
+const getUsers = async (req, res) => {
+    await User.find({}).then((users) => {
+        console.log(users);
+        if (users) {
+            res.status(200).json({"message": "users found", "users": users});
+        } else {
+            res.status(400).json({"message": "no users found"});
+        }
+    }).catch((e) => {
+        console.log(e);
+        res.status(400).json({"message": "something went wrong", "error": e.message});
+    });
+}
+
+module.exports = getUsers;
