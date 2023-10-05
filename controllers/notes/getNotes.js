cd52146d218d3434c7d78fe637b091acc3b7b278
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 12:05:56 +0300

    works on get all notes controller

--- controllers/notes/getNotes.js
+++ controllers/notes/getNotes.js
@@ -1,17 +1,31 @@
 const Note = require("../../models/Note");
+const User = require("../../models/User"); 
 
 const getNotes = async (req, res) => {
     const userId = req.params.userId;
     console.log(userId);
 
     if (userId !== undefined) {
-        User.findById({}).then((user) => {
-
+        User.findById({'_id': userId}).then(async (user) => {
+            if (user) {
+                await Note.find({'owner': userId}).then((notes) => {
+                    if (notes) {
+                        res.status(200).json({"message": "notes retreived", "notes": notes});
+                    } else {
+                        res.status(400).json({"message": "no notes found"});
+                    }
+                }).catch((e) => {
+                    console.log(e);
+                    res.status(400).json({"message": "somethig went wrong", "error": e.message});
+                });
+            } else {
+                res.status(400).json({"message": "no user found"})
+            }
         }).catch((e) => {
             console.log(e);
             res.status(400).json({"message": "something went wrong", "error": e.message});
         });
-        await Note.find({'owner': userId});
+        
     } else {
         res.status(400).json({"message": "please provide a valid user Id"});
     }
