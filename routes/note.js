7a1491f4be0e9acf82c6b87b1b48c044f5c40ffc
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 13:05:29 +0300

    adds delete note controller

--- routes/note.js
+++ routes/note.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const addNote = require('../controllers/notes/addNote');
 const getNote = require('../controllers/notes/getNote');
+const deleteNote = require('../controllers/notes/deleteNote');
 
 const noteRoute = express.Router();
 
 noteRoute.post('/v1/notes/:userId', addNote);
 authRoute.get('/v1/notes/:userId/:noteId', getNote);
+authRoute.delete('/v1/notes/:userId/:noteId', deleteNote);
 
 module.exports = noteRoute;
