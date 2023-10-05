63d41c964395e365715bcc8c64d34d4ba79e42a8
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 13:04:09 +0300

    adds get note controller

--- routes/note.js
+++ routes/note.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const addNote = require('../controllers/notes/addNote');
+const getNote = require('../controllers/notes/getNote');
 
 const noteRoute = express.Router();
 
 noteRoute.post('/v1/notes/:userId', addNote);
+authRoute.get('/v1/notes/:userId/:noteId', getNote);
 
 module.exports = noteRoute;
