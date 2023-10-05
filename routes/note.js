52cda6063065e1c8941336616b53538dd989b5a0
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 14:14:02 +0300

    adds update note controller

--- routes/note.js
+++ routes/note.js
@@ -3,12 +3,14 @@ const addNote = require('../controllers/notes/addNote');
 const getNote = require('../controllers/notes/getNote');
 const deleteNote = require('../controllers/notes/deleteNote');
 const getNotes = require('../controllers/notes/getNotes');
+const updateNote = require('../controllers/notes/updateNote');
 
 const noteRoute = express.Router();
 
 noteRoute.post('/v1/notes/:userId', addNote);
 noteRoute.get('/v1/notes/:userId', getNotes);
 noteRoute.get('/v1/notes/:userId/:noteId', getNote);
+authRoute.put('/v1/notes/:userId/:noteId', updateNote);
 noteRoute.delete('/v1/notes/:userId/:noteId', deleteNote);
 
 module.exports = noteRoute;
