995c69399734d30610ad4b1e662586544aad7ac8
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 15:13:53 +0300

    adds delete user controller

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const getUsers = require('../controllers/user/getUsers');
 const getUser = require('../controllers/user/getUser');
+const deleteUser = require('../controllers/user/deleteUser');
 
 const userRoute = express.Router();
 
 userRoute.get('/v1/users', getUsers);
 userRoute.get('/v1/users/:userId', getUser);
+userRoute.delete('/v1/users/:userId', deleteUser);
 
 module.exports = userRoute;
