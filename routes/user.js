2c4f0eae2d77aabcb65371a2a09eff697239cbaa
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 16:03:44 +0300

    adds update user controller

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 const getUsers = require('../controllers/user/getUsers');
 const getUser = require('../controllers/user/getUser');
 const deleteUser = require('../controllers/user/deleteUser');
+const updateUser = require('../controllers/user/updateUser');
 
 const userRoute = express.Router();
 
 userRoute.get('/v1/users', getUsers);
 userRoute.get('/v1/users/:userId', getUser);
+userRoute.put('/v1/users/:userId', updateUser);
 userRoute.delete('/v1/users/:userId', deleteUser);
 
 module.exports = userRoute;
