b356f3e8f2f01efd7cbdc2662d2bac4fd3c81adc
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 14:12:09 +0300

    works on update note controller

---
+++ controllers/notes/updateNote.js
@@ -0,0 +1,37 @@
+const Note = require("../../models/Note");
+
+const updateNote = async (req, res) => {
+    const { status } = req.body;
+    const userId = req.params.userId;
+    console.log(userId);
+    const noteId = req.params.noteId;
+    console.log(noteId);
+
+    if (userId !== undefined && noteId !== undefined) {
+        await User.findById({ '_id': userId }).then(async (user) => {
+            if (user) {
+                await Note.findByIdAndUpdate({ '_id': noteId }, { $set: { "status": status } }, { new: true }).then((note) => {
+                    console.log(note);
+                    if (note) {
+                        res.status(201).json({ "message": "note updated successfully", note });
+                    } else {
+                        res.status(400).json({ "message": "failed to update note" });
+                    }
+                }).catch((e) => {
+                    console.log(e);
+                    res.status(400).json({ "message": "something went wrong", "error": e.message });
+                });
+            } else {
+                res.status(400).json({ "message": "no user found" });
+            }
+        }).catch((e) => {
+            console.log(e);
+            res.status(400).json({ "message": "something went wrong", "error": e.message });
+        });
+    } else {
+        res.status(400).json({ "message": "please provide valid user and note Id" });
+    }
+
+}
+
+module.exports = updateNote;
--- routes/note.js
+++ routes/note.js
@@ -2,10 +2,12 @@ const express = require('express');
 const addNote = require('../controllers/notes/addNote');
 const getNote = require('../controllers/notes/getNote');
 const deleteNote = require('../controllers/notes/deleteNote');
+const getNotes = require('../controllers/notes/getNotes');
 
 const noteRoute = express.Router();
 
 noteRoute.post('/v1/notes/:userId', addNote);
+noteRoute.get('/v1/notes/:userId', getNotes);
 noteRoute.get('/v1/notes/:userId/:noteId', getNote);
 noteRoute.delete('/v1/notes/:userId/:noteId', deleteNote);
 
