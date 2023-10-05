78ced70d4bdef36549dbdaf9cf8227d5f9026782
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 16:34:13 +0300

    finishes up on update user controller

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const userRoute = express.Router();
 
 userRoute.get('/v1/users', getUsers);
 userRoute.get('/v1/users/:userId', getUser);
-userRoute.put('/v1/users/:userId', updateUser);
+userRoute.put('/v1/users/:userId', updateUser); 
 userRoute.delete('/v1/users/:userId', deleteUser);
 
 module.exports = userRoute;
