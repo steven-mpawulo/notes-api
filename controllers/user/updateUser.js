f5049b4a4961bf2646be46fb284b0e1b694bbcac
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-05 15:59:32 +0300

    works on update user controller

--- controllers/user/updateUser.js
+++ controllers/user/updateUser.js
@@ -8,18 +8,18 @@ const updateUser = async (req, res) => {
 
     if (userId !== undefined) {
         if (email) {
-
+            await User.findByIdAndUpdate({ '_id': userId }, { $set: { "email": email } }, { new: true }).then((user) => {
+                console.log(user);
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({ "message": "something went wrong", "error": e.message });
+            });
         } else {
-            res.status(400).json({"message": "please provide a valid email address"});
+            res.status(400).json({ "message": "please provide a valid email address" });
         }
-        await User.findByIdAndUpdate({'_id': userId }, {$set: {"email": email}}, {new: true}).then((user) => {
-            console.log(user);
-        }).catch((e) => {
-            console.log(e);
-            res.status(400).json({"message": "something went wrong", "error": e.message});
-        } );
+
     } else {
-        res.status(400).json({"message": "please provide valid user Id"});
+        res.status(400).json({ "message": "please provide valid user Id" });
     }
 }
 
