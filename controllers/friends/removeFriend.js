29c9e8ee54d604f76f896e9ac8494442ecd2d10d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-06 12:54:38 +0300

    works on removing friend controller

---
+++ controllers/friends/removeFriend.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const removeFriend = (req, res) => {
+	const friendId = req.params.friendId;
+	console.log(friendId);
+	const userId = req.params.userId;
+	console.log(userId);
+
+	if (friendId !== undefined && userId !== undefined) {
+		await User.findByIdAndUpdate({'_id': userId}, {$pull: {'friends': friendId}}, {new: true}).then((updatedUser) => {
+			if (updatedUser) {
+				await User.findByIdAndUpdate({'_id': friendId}, {$pull: {'friends': userId}}, {new: true}).then((updatedFriend) => {
+					console.log(updatedFriend);
+					if (updatedFriend) {
+						res.status(200).json({"message": "friend removed"});
+					} else {
+					res.status(400).json({"message": "failed to friend removed"});	
+					}
+					
+				}).catch((e) => {
+					console.log(e);
+					res.status(400).json({"message": "something went wrong", "error": e.message});
+				});
+			} else {
+				res.status(400).json({"message": "failed to update user friend list"});
+			}
+		}).catch((e) => {
+			console.log(e);
+			res.status(400).json({"message": "something went wrong", "error": e.message});
+		});
+	} else {
+		res.status(400).json({"message": "please provide valid friend and user Id"});
+	}
+}
