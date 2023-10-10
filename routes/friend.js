4b3fb864234b7a00f691bc360778a7b6e0d1797e
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-10 10:04:22 +0300

    adds get friend controller to friend route

--- routes/friend.js
+++ routes/friend.js
@@ -4,9 +4,11 @@ const friendRoute = express.Router();
 const addFriend = require('../controllers/friends/addFriend');
 const removeFriend = require('../controllers/friends/removeFriend');
 const getFriends = require('../controllers/friends/getFriends');
+const getFriend = require('../controllers/friends/getFriend');
 
 friendRoute.post('/v1/friends/:friendId/:userId', addFriend);
 friendRoute.get('/v1/friends/:userId', getFriends);
+friendRoute.get('/v1/friends/:userId/:friendId', getFriend);
 
 friendRoute.delete('/v1/friends/:friendId/:userId', removeFriend);
 
